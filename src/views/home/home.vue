a07ec753dae689c002ecf31e784340938b9a9627
Julia Shamsutdinova <shjn0297@gmail.com>
2021-09-25 22:40:54 +0800

    Added a new image

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,14 +1,11 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="8" lg="8">
+      <v-col cols="12" md="12" lg="8">
         <abstract-text />
       </v-col>
-      <v-col cols="12" md="4" lg="4">
-        <div class='plots'>
-          <img src="@/assets/plots/plot_1.png" alt="plot_1" class="pb-3">
-          <img src="@/assets/plots/plot_2.png" alt="plot_2">
-        </div>
+      <v-col cols="12" lg="4">
+          <img src="@/assets/plots/SRHvsLOFAR1.png" alt="SRH vs LOFAR">
       </v-col>
     </v-row>
   </v-container>
