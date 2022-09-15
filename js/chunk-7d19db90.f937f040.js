ea27579f7b0045a0c84b32e4e96ebeb82b5355c5
jshamsutdinova <shjn0297@gmail.com>
2022-09-15 14:09:56 +0800

    New Deployment

---
+++ js/chunk-7d19db90.f937f040.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7d19db90"],{"0799":function(n,e,t){},a162:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-container")},c=[],i={name:"highlights"},u=i,o=(t("b5ef"),t("2877")),r=t("6544"),s=t.n(r),f=t("a523"),l=Object(o["a"])(u,a,c,!1,null,"42f441ba",null);e["default"]=l.exports;s()(l,{VContainer:f["a"]})},b5ef:function(n,e,t){"use strict";t("0799")}}]);
+//# sourceMappingURL=chunk-7d19db90.f937f040.js.map
